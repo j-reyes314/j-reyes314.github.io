97c8e6f0baadfe875ded5896f1f18d17d50311e3
Joshua Reyes <j_reyes314@yahoo.com>
2021-12-14 20:47:43 -0500

    copy over files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,103 @@
 import logo from './logo.svg';
 import './App.css';
+import Contact from './components/contact/Contact';
+import ProjectCard from './components/projectCard/ProjectCard';
+import { useRef } from 'react';
 
 function App() {
+  const contactRef = useRef();
+
+  function scrollContact() {
+    contactRef.current.scrollIntoView({ behavior: 'smooth' })
+}
+  
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+
+        
       </header>
+
+      <div className ="ribbon">
+        <div className ="blue"></div>
+        <div className = "green"></div>
+        <div className ="yellow"></div>
+        <div className ="orange"></div>
+        <div className = "red"></div>
+      </div>
+
+
+      <div className="body">
+
+      
+        <div className='intro'>
+          <div style={{ display: 'relative', textAlign: "left"  }}>
+            <p style ={{
+              position: 'absolute', 
+              fontSize: '6em', 
+              margin:0,
+              fontFamily: 'Brush Script MT', 
+              left: '10px', 
+              top: '100px'
+              }}
+            >
+              Hello! I'm
+            </p>
+          </div>
+            
+          <p 
+            style={{ 
+              color: "black",
+              transform: "matrix(1, -0.20, 0, 1, 0, 0)",
+              msTransform: "matrix(1, -0.20, 0, 1, 0, 0)",
+              WebkitTransform: "matrix(1, -0.20, 0, 1, 0, 0)",
+              textShadow: "4px 5px #e6e6d8, 6px 7px #c6a39a",
+              fontSize: '8em',
+              marginTop: '2em',
+              fontFamily: 'Lucida Console'
+            }}
+          >
+            Joshua Reyes
+          </p>
+        </div>
+          
+        <div className='about'>
+
+          <h1 style ={{color: "black"}}>A little bit about me...</h1>
+          <p style={{fontSize: '2em'}}>
+            I am a current graduating senior at Lehman College. I formerly worked as a freelance technician in various theatres
+            throughout New York City. I will soon be earning a BS in Computer Science. I have a passion for Frontend development and
+            creating great user experiences. I am new to this profession and am always excited to learn new software and skills. 
+          </p>
+           {/* <p onClick={scrollContact} className="contact">Contact me here!</p> */}
+
+        </div>
+          
+
+         
+
+        <div className="portfolio">
+
+          <h1 style ={{color: "black"}}>My Portfolio</h1>
+          <div style={{width: "99%", display: 'grid', gridTemplateColumns: "repeat(3,1fr)", gap: '18px'}}>
+            
+            <ProjectCard message ="Check out the Weather!" clr ={0} link ="https://j-reyes314.github.io/my-weather-app/"/>
+            <ProjectCard message ="Figure out the fruit with Machine Learning!" clr ={1} link ="https://github.com/j-reyes314/Project_Examples/blob/main/Fruits_Logistic_Regression.ipynb"/>
+            <ProjectCard message ="Check out this code!" clr={2} link ="https://github.com/ChrisEspinal/my-group-project2"/>
+          
+          </div>
+
+        </div>
+
+
+        
+        <div ref ={contactRef}>
+          <Contact/>
+        </div>
+        
+      </div>
     </div>
   );
 }
---
+++ src/components/contact/Contact.js
@@ -0,0 +1,73 @@
+// import GitHubButton from "react-github-button"
+import { useState } from 'react';
+import './contact.css';
+
+export default function Contact(props) {
+    const [show, setShow] = useState('snackbar');
+    const [inputs,setInputs] = useState({});
+
+    const handleChange = (event) => {
+        const name = event.target.name;
+        const value = event.target.value;
+
+        setInputs(values => ({...values, [name]: value}));
+    }
+
+    function handleSubmit (event) {
+        event.preventDefault()
+
+        
+
+        // setShow('snackbar show');
+
+        // setTimeout(setShow('snackbar'), 3000);
+
+        alert('Message sent!')
+        setInputs({});
+    }
+    return (
+        <>
+            <div style={{textAlign:'left'}}>
+                <h1 style={{color: "black", textAlign: 'left'}}>Contact me</h1>
+                <p style={{fontSize: '2em'}}>I'd love to meet you. Why not send an email?</p>
+            </div>
+
+
+            <form action="" onSubmit={handleSubmit}>
+                <div className ='row input-container'>
+
+                    <div className='styled-input wide'>
+                        <input type="text" name='name' value={inputs.name || ''} onChange={handleChange} required/>
+                        <label>Name</label>
+                    </div>
+
+                    <div className='styled-input' style={{paddingRight: '1em'}}>
+                        <input  type="text" name ="email" value={inputs.email || ''} onChange={handleChange} required/>
+                        <label>Email</label>
+                    </div>
+
+                    <div className='styled-input'>
+                        <input style={{marginLeft: '3em'}} type="text" name= "subject" value={inputs.subject || ''} onChange={handleChange} required/>
+                        <label style={{marginLeft: '3em'}}>Subject</label>
+                    </div>
+
+                    <div className='styled-input wide'>
+                        <textarea type="text" name= "message" value={inputs.message || ''} onChange={handleChange} required/>
+                        <label>Message</label>
+                    </div>
+
+                    <div className='styled-input wide btn'>
+                        <input type="submit" value="submit" />
+                    </div>
+
+                </div>
+            </form>
+
+            <div className ={show}>Message sent!</div>
+
+
+
+            {/* <GitHubButton href="https://github.com/j-reyes314"></GitHubButton> */}
+        </>
+    )
+}
